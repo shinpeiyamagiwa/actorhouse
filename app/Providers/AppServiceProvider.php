5cee881165bdb5f892b423677e81da580e143a18
shinpei <giwapei@gmail.com>
2019-06-29 21:54:42 +0900

    heroku用編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //本番環境でHttpsを使用する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
