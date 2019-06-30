3eab46e5e78302c53e6d64d2ef78a0e41c928328
shinpei <giwapei@gmail.com>
2019-06-30 13:14:29 +0900

    俳優update変更（youtubeAPI削除）

--- app/Http/Controllers/ActorUpdateController.php
+++ app/Http/Controllers/ActorUpdateController.php
@@ -29,21 +29,6 @@ class ActorUpdateController extends Controller
             curl_close($curl);
             
             if(isset($response['id'])) {
-                // Youtube
-                $option = array(
-                    'part' => 'snippet', 
-                    'key' => 'AIzaSyC23guRfvzzJOq4XhSu-8DDWBuj3yPwHfk',
-                    'q' => $response['name']
-                 );
-                $url = "https://www.googleapis.com/youtube/v3/search?".http_build_query($option, 'a', '&');
-                $curl = curl_init($url);
-                curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
-                $json_response = curl_exec($curl);
-                curl_close($curl);
-                $responseObj = json_decode($json_response, true);
-                
                 // 重複防止
                 $actor = Actor::where('tmdb_id', $response['id'])->first();
                 if(is_null($actor)){
@@ -52,7 +37,7 @@ class ActorUpdateController extends Controller
                         'tmdb_id' => $response['id'],
                         'image_path' => $response['profile_path'],
                         'birthday' => $response['birthday'],
-                        'video_link' => isset($responseObj['items'][0]['id']['videoId']) ? $responseObj['items'][0]['id']['videoId'] : 'test',
+                        'video_link' => null,
                         'twitter_link' => 'test',
                         'place' => $response['place_of_birth'],
                         'homepage' => $response['homepage'],
