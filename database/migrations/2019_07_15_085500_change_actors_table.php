da778e2cf6d2daded05341cc14a32e3bffdfe601
shinpei <giwapei@gmail.com>
2019-07-15 18:08:56 +0900

    actor index変更

---
+++ database/migrations/2019_07_15_085500_change_actors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actors', function (Blueprint $table) {
+            //
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actors', function (Blueprint $table) {
+            //
+        });
+    }
+}
