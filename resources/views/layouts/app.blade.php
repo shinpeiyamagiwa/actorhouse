84e509b99b432121349f97221813c51236be6652
shinpei <giwapei@gmail.com>
2019-07-22 20:50:03 +0900

    プレビュー機能

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,16 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     
-    <meta property=”og:title” content=”ActorHOUSE” />
+    <meta property="og:type" content="article" />
+    <meta property=”og:title” content=”ActorHOUSE”>
     <meta property=”og:image” content=”images/表紙.png”>
-    <meta property=”og:description” content=”俳優好きに特化した映画記録アプリ。人生の映画鑑賞の記録をつけて好きな俳優を見つけよう！” />
+    <meta property=”og:description” content=”俳優好きに特化した映画記録アプリ。人生の映画鑑賞の記録をつけて好きな俳優を見つけよう！”>
+    <meta name="twitter:card" content="summary" />
     <!-- Scripts -->
     <script src="{{ asset('js/app.js', true) }}"></script>
 
