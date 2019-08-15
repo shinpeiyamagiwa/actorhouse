57b7192223445bbf9c40625a98f8b8cb56f12562
shinpei <giwapei@gmail.com>
2019-08-15 18:47:22 +0900

    評価スライダー表示数値変化対応

--- resources/views/actor/index.blade.php
+++ resources/views/actor/index.blade.php
@@ -712,4 +712,14 @@
     });
   });
 </script>
+<script>
+    var elem = document.getElementById('range');
+    var target = document.getElementById('value');
+    var rangeValue = function (elem, target) {
+      return function(evt){
+        target.innerHTML = elem.value;
+      }
+    }
+    elem.addEventListener('input', rangeValue(elem, target));
+  </script>
 @endsection
